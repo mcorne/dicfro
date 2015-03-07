d6ff4531379314217c7fabcd27e36e4657c9e0d4
Michel Corne <miccorne@gmail.com>
2015-03-07 12:54:48 +0100

    fixed renard meon2 glossary

--- application/config.php
+++ application/config.php
@@ -597,32 +597,6 @@ return array(
             'volume'         => 'readonly',
         ),
 
-        'renart-meon-2' => array(
-            'availability-test' => 'adolé',
-            'created'        => '2012-01-07',
-            'description'    => 'Glossaire du Roman de Renart, volume 2, M. D. M. Méon, 1826',
-            'description-en' => 'Glossary of the Roman de Renart, volume 2, M. D. M. Méon, 1826',
-            'image'          => 'glossaire-roman-de-renart-meon-vol2.jpg',
-            'language'       => 'fr',
-            'name'           => 'Roman de Renart / Méon v.2',
-            'parser'         => array(
-                'class'      => 'Model_Parser_GaffiotLike',
-                'properties' => array(
-                    'imageNumberTpl' => '0000%s',
-                    'lineTpl'        => '~^(.+?);(\d+)~',
-                )
-            ),
-            'search'         => array(
-                'properties' => array(
-                    'needTcaf'   => true,
-                    'needTobler' => true,
-                )
-            ),
-            'title'          => 'Méon 2',
-            'type'           => 'internal',
-            'url'            => 'glossaire-roman-de-renart-meon-vol2',
-        ),
-
         'renart-meon-3' => array(
             'availability-test' => 'aignel',
             'created'        => '2012-01-07',
@@ -753,6 +727,33 @@ return array(
             'title'          => 'Méon 1',
             'type'           => 'internal',
             'url'            => 'glossaire-roman-de-renart-meon-vol1',
+        ),
+
+        'roman-de-renart-meon-2-glossary' => array(
+            'availability-test' => 'adolé',
+            'created'        => '2012-01-07',
+            'description'    => 'Glossaire du Roman de Renart, volume 2, M. D. M. Méon, 1826',
+            'description-en' => 'Glossary of the Roman de Renart, volume 2, M. D. M. Méon, 1826',
+            'image'          => 'glossaire-roman-de-renart-meon-vol2.jpg',
+            'language'       => 'fr',
+            'name'           => 'Roman de Renart / Méon v.2',
+            'parser'         => array(
+                'class'      => 'Model_Parser_GaffiotLike',
+                'properties' => array(
+                    'imageNumberTpl' => '0000%s',
+                    'lineTpl'        => '~^(.+?);(\d+)~',
+                )
+            ),
+            'search'         => array(
+                'properties' => array(
+                    'imagePath'  => 'old-french-glossaries/roman-de-renart-meon-2-glossary/mImg/%s.gif',
+                    'needTcaf'   => true,
+                    'needTobler' => true,
+                )
+            ),
+            'title'          => 'Méon 2',
+            'type'           => 'internal',
+            'url'            => 'glossaire-roman-de-renart-meon-vol2',
         ),
 
         'roman-lexicon' => array(
@@ -973,7 +974,7 @@ return array(
                 'rose',
                 'roman-de-renart-fhs-glossary',
                 'roman-de-renart-meon-1-glossary',
-                'renart-meon-2',
+                'roman-de-renart-meon-2-glossary',
                 'renart-meon-3',
                 'renart-meon-4',
                 'tristan',
